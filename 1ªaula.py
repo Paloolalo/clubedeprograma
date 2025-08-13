d5311fba9b2120db054b3254c968cfb5a77f4f2d
palo <m41099549@gmail.com>
2025-08-13 14:51:47 -0300

    Create 1ªaula.py

---
+++ 1ªaula.py
@@ -0,0 +1,27 @@
+nome = 'Paloo'
+idade= 14
+turma= 'F'
+
+
+print ('meu nome é '+ nome)
+print ('eu tenho ' + str (idade) + ' anos')
+
+num1 =3
+num2 =11
+num3 =10
+num4 =3 
+resultado =  0
+
+resultado = (num1 + num2 + num3 + num4) / 4  
+
+
+print (resultado)
+
+nota1 = int (input('digite a primeira nota:'))
+nota2 = int (input('digite a segunda nota:'))
+nota3 = int (input('digite a terceita nota:'))
+nota4 = int (input('digite a quarta nota:'))
+
+resultado2 = (nota1 + nota2 + nota3 +nota4 )  /4  
+
+print ('a média é:' + str  (resultado2))
