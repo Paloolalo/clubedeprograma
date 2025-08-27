50c134f7fde718c882a109d7eb8e36bde4d3f14c
palo <m41099549@gmail.com>
2025-08-27 15:03:55 -0300

    Create 3ª aula_lista.py

---
+++ 3ª aula_lista.py
@@ -0,0 +1,13 @@
+frutas = ['manga', 'morango', 'maçã', 'banana', 'uva','kibe', 'limão', 'mirtilo']
+
+frutas.append('melão') 
+
+
+frutas.append('melancia') 
+
+
+frutas.remove('kibe')
+
+
+for fruta in frutas:
+    print (fruta)
